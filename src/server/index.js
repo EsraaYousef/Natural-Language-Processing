954ab15b8444086474a666640e2ab77b6cb7a87f
exit <esraayousef400@gmail.com>
2021-04-19 10:14:38 +0200

    fix service-worker issue

--- src/server/index.js
+++ src/server/index.js
@@ -12,7 +12,7 @@ app.use(express.static("dist"));
 console.log(__dirname);
 
 app.get("/", function (req, res) {
-  res.sendFile("dist/index.html");
+  res.sendFile("index.html");
   // res.sendFile(path.resolve("dist/index.html"));
 });
 
